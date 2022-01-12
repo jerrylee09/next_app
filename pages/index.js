e41b3f1cd74b83ecfa99e6ee30268bf0da6d59a3
jerry lee <jerry.lee09@yahoo.com>
2022-01-11 21:44:56 -0600

    added card styles and components

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
-export default ()=> {
-    return <p1>hello world</p1>
-}
+import styles from './index.module.css';
+import Card from './Card';
+
+export default () => {
+    return (
+        <div className={styles.app}>
+            <header className={styles.header}>
+                <img src="/logo.png"
+                    className={styles.logo} alt="logo"
+                />
+            </header>
+            <div className={styles.grid}>
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div>
+    )
+};
